d909178f3dcdf804c9f3645046bbc6d6a4ad73ac
Nel-Medeiros <emanuelpaivamedeiros@gmail.com>
2020-09-13 16:49:55 -0300

    DB persistence, Validation and Test Coverage

--- lib/voting/create_admin.ex
+++ lib/voting/create_admin.ex
@@ -5,13 +5,14 @@ defmodule Voting.CreateAdmin do
 
   import Ecto.Changeset
 
-  alias Voting.Admin
+  alias Voting.{Admin, Repo}
 
   def run(params) do
     %Admin{}
     |> cast(params, [:name, :email, :password])
     |> validate_required([:name, :email, :password])
     |> put_password()
+    |> Repo.insert()
   end
 
   defp put_password(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
---
+++ test/voting/create_admin_test.exs
@@ -0,0 +1,34 @@
+defmodule Voting.CreateAdminTest do
+
+  use Voting.DataCase, async: true
+
+  alias Voting.{Admin, CreateAdmin}
+
+  describe "run/1" do
+    test "returns a struct when the params are valid" do
+      params = %{name: "Testson", email: "testson2020@test.com", password: "ilovepudim"}
+      assert {:ok, %Admin{} = admin} = CreateAdmin.run(params)
+      assert admin.name == "Testson"
+      assert admin.email == "testson2020@test.com"
+      assert admin.password_hash != "ilovepudim"
+    end
+
+    test "returns error when name is missing" do
+      params = %{name: "", email: "testson2020@test.com", password: "ilovepudim"}
+      assert {:error, %Ecto.Changeset{} = changeset} = CreateAdmin.run(params)
+      %{name: ["can't be blank"]} = errors_on(changeset)
+    end
+
+    test "returns error when email is missing" do
+      params = %{name: "Testson", email: "", password: "ilovepudim"}
+      assert {:error, %Ecto.Changeset{} = changeset} = CreateAdmin.run(params)
+      %{email: ["can't be blank"]} = errors_on(changeset)
+    end
+
+    test "returns error when password is missing" do
+      params = %{name: "Testson", email: "testson2020@test.com", password: ""}
+      assert {:error, %Ecto.Changeset{} = changeset} = CreateAdmin.run(params)
+      %{password: ["can't be blank"]} = errors_on(changeset)
+    end
+  end
+end
